e850813ea0c6ce5e793f7b9e86b032c529264ded
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2024-04-29 16:25:36 +0300

    publish add acess to push artifacts

--- publish.sh
+++ publish.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://europe-west4-docker.pkg.dev
 docker build . -t europe-west4-docker.pkg.dev/hivenote/inbest-task/inbest-task-frontend
 docker push europe-west4-docker.pkg.dev/hivenote/inbest-task/inbest-task-frontend
