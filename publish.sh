4a52cdf88ce3fb9e617b787a98915c222fda814c
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2024-04-29 17:00:33 +0300

    restore changes

--- publish.sh
+++ publish.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://europe-west4-docker.pkg.dev
 docker build . -t europe-west4-docker.pkg.dev/hivenote/inbest-task/inbest-task-frontend
 docker push europe-west4-docker.pkg.dev/hivenote/inbest-task/inbest-task-frontend
